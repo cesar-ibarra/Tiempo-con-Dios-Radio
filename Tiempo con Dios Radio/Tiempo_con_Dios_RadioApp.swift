654dfd58d7e49182c63f387b8c71840cb08a77cc
Cesar Ibarra <ibarratcesar@icloud.com>
2024-10-02 21:08:10 -0700

    Initial Commit

---
+++ Tiempo con Dios Radio/Tiempo_con_Dios_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiempo_con_Dios_RadioApp.swift
+//  Tiempo con Dios Radio
+//
+//  Created by Cesar Ibarra on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Tiempo_con_Dios_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
